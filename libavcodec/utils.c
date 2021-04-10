a44e3d738535f2124e30b99bdbd3fc8b92480b64
Michael Niedermayer <michael@niedermayer.cc>
2021-04-10 20:32:55 +0200

    avcodec/utils: Check ima wav duration for overflow
    Fixes: signed integer overflow: 44331634 * 65 cannot be represented in type 'int'
    Fixes: 32120/clusterfuzz-testcase-minimized-ffmpeg_dem_RSD_fuzzer-5760221223583744
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Change-Id: I53ddd95e091b250b825080887443bee8c5d2b0e6
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit f40e9b13554d88cbdd6cd2b4a3da2cbea9590f5d)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -3195,11 +3195,15 @@ int av_get_audio_frame_duration(AVCodecContext *avctx, int frame_bytes)
             if (ba > 0) {
                 /* calc from frame_bytes, channels, and block_align */
                 int blocks = frame_bytes / ba;
+                int64_t tmp;
                 switch (avctx->codec_id) {
                 case AV_CODEC_ID_ADPCM_IMA_WAV:
                     if (bps < 2 || bps > 5)
                         return 0;
-                    return blocks * (1 + (ba - 4 * ch) / (bps * ch) * 8);
+                    tmp = blocks * (1LL + (ba - 4 * ch) / (bps * ch) * 8);
+                    if (tmp != (int)tmp)
+                        return 0;
+                    return tmp;
                 case AV_CODEC_ID_ADPCM_IMA_DK3:
                     return blocks * (((ba - 16) * 2 / 3 * 4) / ch);
                 case AV_CODEC_ID_ADPCM_IMA_DK4:
