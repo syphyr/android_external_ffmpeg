wiseplay-app/ijkplayer-ffmpeg

avformat/rmdec: Clear extradata when extradata_size is cleared
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/rmdec.c
@@ -207,6 +207,7 @@ static int rm_read_audio_stream_info(AVFormatContext *s, AVIOContext *pb,
             break;
         case AV_CODEC_ID_RA_288:
             st->codecpar->extradata_size= 0;
+            av_freep(&st->codecpar->extradata);
             ast->audio_framesize = st->codecpar->block_align;
             st->codecpar->block_align = coded_framesize;
             break;

syphyr/android_external_ffmpeg

Change-Id: I1461ea0c301b89becedf7690d3281e07288aa8b4

libavformat/rmdec.c
@@ -211,6 +211,7 @@ static int rm_read_audio_stream_info(AVFormatContext *s, AVIOContext *pb,
             break;
         case AV_CODEC_ID_RA_288:
             st->codec->extradata_size= 0;
+            av_freep(&st->codec->extradata);
             ast->audio_framesize = st->codec->block_align;
             st->codec->block_align = coded_framesize;
             break;