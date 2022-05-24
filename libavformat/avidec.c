FFmpeg/FFmpeg

avformat/avidec: Check height
Fixes: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
Fixes: Ticket8486

Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/avidec.c
@@ -862,6 +862,8 @@ static int avi_read_header(AVFormatContext *s)
                             memcpy(st->codecpar->extradata + st->codecpar->extradata_size - 9,
                                    "BottomUp", 9);
                     }
+                    if (st->codecpar->height == INT_MIN)
+                        return AVERROR_INVALIDDATA;
                     st->codecpar->height = FFABS(st->codecpar->height);
 
 //                    avio_skip(pb, size - 5 * 4);

syphyr/android_external_ffmpeg

(cherry picked from commit ec8ff659f57786c4cb089b07dfeab7e5cbab8d52)
Change-Id: Ic860b62028277b72a87211a9c756212be61974a3

libavformat/avidec.c
@@ -834,6 +834,8 @@ static int avi_read_header(AVFormatContext *s)
                             memcpy(st->codec->extradata + st->codec->extradata_size - 9,
                                    "BottomUp", 9);
                     }
+                    if (st->codec->height == INT_MIN)
+                        return AVERROR_INVALIDDATA;
                     st->codec->height = FFABS(st->codec->height);
 
 //                    avio_skip(pb, size - 5 * 4);