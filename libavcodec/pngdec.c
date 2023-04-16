0f8d9f1a0b961274a02b89c149f88e09b2c93ed2
Michael Niedermayer <michael@niedermayer.cc>
2023-04-16 15:06:59 +0200

    avcodec/pngdec: Do not pass AVFrame into global header decode
    The global header should not contain a frame, and decoding it
    would result in leaks
    
    Fixes: memleak
    Fixes: 45982/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_APNG_fuzzer-6603443149340672
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit d31d4f32283f765c79d6e127d31ee2c37a0acef7)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/pngdec.c
+++ libavcodec/pngdec.c
@@ -620,6 +620,8 @@ static int decode_idat_chunk(AVCodecContext *avctx, PNGDecContext *s,
     int ret;
     size_t byte_depth = s->bit_depth > 8 ? 2 : 1;
 
+    if (!p)
+        return AVERROR_INVALIDDATA;
     if (!(s->hdr_state & PNG_IHDR)) {
         av_log(avctx, AV_LOG_ERROR, "IDAT without IHDR\n");
         return AVERROR_INVALIDDATA;
@@ -1273,6 +1275,10 @@ exit_loop:
         return 0;
     }
 
+    if (!p)
+        return AVERROR_INVALIDDATA;
+
+
     if (s->bits_per_pixel <= 4)
         handle_small_bpp(s, p);
 
@@ -1417,7 +1423,7 @@ static int decode_frame_apng(AVCodecContext *avctx,
         s->zstream.zfree  = ff_png_zfree;
 
         bytestream2_init(&s->gb, avctx->extradata, avctx->extradata_size);
-        if ((ret = decode_frame_common(avctx, s, p, avpkt)) < 0)
+        if ((ret = decode_frame_common(avctx, s, NULL, avpkt)) < 0)
             goto end;
     }
 
