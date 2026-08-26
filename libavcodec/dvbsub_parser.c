37755b9e5ffb55cd6fea3e4327329201e6f6fe6b
Michael Niedermayer <michael@niedermayer.cc>
2026-08-26 20:15:46 +0200

    avcodec/dvbsub_parser: avoid signed overflow in the capacity
    Change-Id: Ib4372987eebfd472386c0a372d168fb5100d6bab

--- libavcodec/dvbsub_parser.c
+++ libavcodec/dvbsub_parser.c
@@ -106,7 +106,7 @@ static int dvbsub_parse(AVCodecParserContext *s,
         }
     }
 
-    if (buf_size - buf_pos + pc->packet_index > PARSE_BUF_SIZE)
+    if (buf_size - buf_pos > PARSE_BUF_SIZE - pc->packet_index)
         return -1;
 
 /* if not currently in a packet, discard data */
